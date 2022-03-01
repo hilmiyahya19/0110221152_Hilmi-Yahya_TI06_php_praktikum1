b0eaef5bc59908cc2d62de76e3df3beff50f5e23
hilmiyahya19 <91936796+hilmiyahya19@users.noreply.github.com>
2022-03-01 10:27:28 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,14 @@
+<?php
+// definisikan variables
+$nama = 'Hilmi Yahya';
+$umur = 18;
+$berat = 50;
+
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+
+echo "<br/>Hello $nama Apakabar";
+echo "<br/> Hai nama saya $nama Umur saya $umur Berat badan saya $berat Kg , Salam kenal semua nya ";
+echo "<hr>";
+?>
